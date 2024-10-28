7f87b2a2ac500e08144ccb353c18af7e5d4111d4
MiguelJulian <julianmiguel.jm@gmail.com>
2024-10-27 22:44:58 -0300

    Agregué los datos para la conexion. Hay que cambiarlos según la base de datos que creemos. Agregué los permisos para que se conecte a internet

---
+++ app/src/main/java/com/example/tpfinal/Conexion/DataBD.java
@@ -0,0 +1,14 @@
+package com.example.tpfinal.Conexion;
+
+public class DataBD {
+
+    public static String host="sql10.freesqldatabase.com";
+    public static String port="3306";
+    public static String nameBD="sql10734922";
+    public static String user="sql10734922";
+    public static String pass="TQjEZPLfBD";
+
+    public static String urlMySQL = "jdbc:mysql://" + host + ":" + port + "/"+nameBD;
+    public static String driver = "com.mysql.jdbc.Driver";
+
+}
